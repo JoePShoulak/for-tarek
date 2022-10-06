166a219914b54ce6fce438233a5f89d4bea0ab71
tarekmn <nytarek@gmail.com>
2022-10-06 18:28:34 -0500

    experimenting with apis

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+//Api request for token
+
+
+
+
+
+
+
+
+
+
+// //Primary getApi function
+// function getApi(event, city) {
+//   var city = input.value
+//   var requestUrl = ``
+
+
+//   //fetch URL convert to json format
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {})
+//   }
