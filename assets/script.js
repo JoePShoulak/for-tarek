a68d9b4f00dd7a67ce6e73322b449c44167f3279
tarekmn <nytarek@gmail.com>
2022-10-10 11:30:21 -0500

    user UI

--- assets/script.js
+++ assets/script.js
@@ -104,15 +104,15 @@ function getPetData(data) {
       <img id= "cardImage" src= " ${element.primary_photo_cropped.large} " alt="dog image" >
       <div class="container saved-group">
         <h4 class="saved-group" ><b>${element.name.length < 8 ? element.name : element.name.slice(0, 8)+'...'}</b></h4>
-        <p class="saved-group2" >${element.breeds.primary}</p>
+        <p class="saved-group2" >${element.breeds.primary.length < 15 ? element.breeds.primary: element.breeds.primary.slice(0, 15)+'..'}</p>
       </div>
       <ul class="list-group saved-group list-group-flush">
         <li class="list-group-item saved-group">${element.age} </li>
         <li class="list-group-item saved-group">${element.distance.toFixed(0)} Miles away</li>
       </ul>
       <div class="card-body saved-group">
-        <a href="#" class="card-link saved-group">Save</a>
-        <a href=${element.url} target="_blank" class="card-link saved-group">Info</a>
+        <button class= "cardButton"><a class = "cardButtonText" href="#" class="card-link saved-group">Save</a></button>
+        <button class= "cardButton"><a class = "cardButtonText" href=${element.url} target="_blank" class="card-link saved-group">Info</a></button>
       </div>
     </div>`)
       
